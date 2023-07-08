670154542aa47a552155a1b3c739e68246598126
Olalken Famoroti <lekanfamoroti@gmail.com>
2023-07-08 14:45:04 -0500

    created autoscaling group file

--- alb.tf
+++ alb.tf
@@ -3,21 +3,21 @@
 resource "aws_lb" "application_load_balancer" {
   name               = "dev-alb"
   load_balancer_type = "application"
-  internal = false
-  security_groups = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.public_subnet_az1.id, aws_subnet.public_subnet_az2.id]  
+  internal           = false
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_subnet_az1.id, aws_subnet.public_subnet_az2.id]
 
   tags = {
-    Name ="dev-alb"
+    Name = "dev-alb"
   }
 }
 
 resource "aws_lb_target_group" "tg" {
-  name     = "dev-tg"
+  name        = "dev-tg"
   target_type = "instance"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.vpc.id  
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.vpc.id
 
   health_check {
     path                = "/health"
@@ -25,9 +25,9 @@ resource "aws_lb_target_group" "tg" {
     interval            = 30
     timeout             = 10
     healthy_threshold   = 3
-    port = "traffic-port"
+    port                = "traffic-port"
     unhealthy_threshold = 3
-    }
+  }
 }
 
 
@@ -36,7 +36,7 @@ resource "aws_lb_listener" "https_listener" {
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn  = "arn:aws:acm:us-east-1:732025887430:certificate/fe172d4a-7b60-4928-99f7-91fa4a83be08"
+  certificate_arn   = "arn:aws:acm:us-east-1:732025887430:certificate/fe172d4a-7b60-4928-99f7-91fa4a83be08"
 
 
 
@@ -52,11 +52,11 @@ resource "aws_lb_listener" "http_listener" {
   protocol          = "HTTP"
 
   default_action {
-    type             = "redirect"
+    type = "redirect"
     redirect {
-      protocol      = "HTTPS"
-      port          = "443"
-      status_code   = "HTTP_301"
+      protocol    = "HTTPS"
+      port        = "443"
+      status_code = "HTTP_301"
     }
   }
 }
---
+++ asg.tf
@@ -0,0 +1,41 @@
+
+resource "aws_launch_template" "launch_template" {
+  name            = "dev-asg"
+  image_id        = "ami-09e0cf8dede3e542a"                                      # Replace with the desired AMI ID
+  instance_type   = "t2.micro"                                                   # Replace with the desired instance type
+  vpc_security_group_ids = [aws_security_group.ssh_sg.id, aws_security_group.alb_sg.id] # Replace with the desired security group ID(s)
+  key_name        = "mykey"
+  description     = "asg for launch template"
+}
+
+resource "aws_autoscaling_group" "auto_sg" {
+  vpc_zone_identifier       = [aws_subnet.private_app_subnet_az1.id, aws_subnet.private_app_subnet_az1.id]
+  name                      = "dev-asg"
+  min_size                  = 1
+  max_size                  = 3
+  desired_capacity          = 2
+  health_check_type         = "ELB"
+  health_check_grace_period = 300
+
+  launch_template {
+    name    = aws_launch_template.launch_template.name
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "asg-webserver"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    ignore_changes = [target_group_arns]
+  }
+
+}
+
+
+resource "aws_autoscaling_attachment" "alb_target_group_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.auto_sg.name
+  lb_target_group_arn   = aws_lb_target_group.tg.arn
+}
